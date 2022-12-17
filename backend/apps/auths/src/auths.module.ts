50033e9af8ee746f62660ad5ae331e724f01815b
Nvmanh472001 <nguyenvanmanh_t64@hus.edu.vn>
2022-12-17 17:12:28 +0700

    add auth services

---
+++ backend/apps/auths/src/auths.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AuthsController } from './auths.controller';
+import { AuthsService } from './auths.service';
+
+@Module({
+  imports: [],
+  controllers: [AuthsController],
+  providers: [AuthsService],
+})
+export class AuthsModule {}
