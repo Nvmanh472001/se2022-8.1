d211944535c47cbbcf8aa68b8477ce740c251ee8
Nvmanh472001 <nguyenvanmanh_t64@hus.edu.vn>
2023-02-05 22:34:32 +0700

    add route for api and add docker file

---
+++ backend/files/src/main.ts
@@ -0,0 +1,63 @@
+import {
+  ClassSerializerInterceptor,
+  Logger,
+  ValidationPipe,
+} from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { ConfigService } from './config/config.service';
+import { HttpExceptionFilter, ResponseInterceptor } from './core/interceptor';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+function configureSwagger(app): void {
+  const config = new DocumentBuilder()
+    .setTitle('files')
+    .setDescription('API Description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, document);
+}
+
+async function bootstrap() {
+  const server = express();
+  const logger = new Logger();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server), {
+    bufferLogs: true,
+    cors: true,
+  });
+  const configService = app.get(ConfigService);
+  const moduleRef = app.select(AppModule);
+  const reflector = moduleRef.get(Reflector);
+  app.useGlobalInterceptors(
+    new ResponseInterceptor(reflector),
+    new ClassSerializerInterceptor(reflector),
+  );
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  configureSwagger(app);
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: [`${configService.get('rb_url')}`],
+      queue: `${configService.get('files_queue')}`,
+      queueOptions: { durable: false },
+      prefetchCount: 1,
+    },
+  });
+  await app.startAllMicroservices();
+  await app.listen(configService.get('servicePort'));
+  logger.log(
+    `🚀 Files service running on port ${configService.get('servicePort')}`,
+  );
+}
+bootstrap();
